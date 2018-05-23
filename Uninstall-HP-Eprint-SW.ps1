d8202d25217ec1f583e4f4c18a3b37e8637f3bf0
DividedCode <32494831+DividedCode@users.noreply.github.com>
2018-05-23 09:35:53 +0200

    functions

--- Uninstall-HP-Eprint-SW.ps1
+++ Uninstall-HP-Eprint-SW.ps1
@@ -1,10 +1,29 @@
-$path = Get-ChildItem -Path "C:\ProgramData\Package Cache\" -Recurse -Filter "HPEPrintAppSetup.exe"
+
+function HPEPrintAppSetupInstalled {
+    $path = Get-ChildItem -Path "C:\ProgramData\Package Cache\" -Recurse -Filter "HPEPrintAppSetup.exe"
+    $path
+}
+function UninstallHPEPrintAppSetupInstalled {
+    Start-Process $path.FullName -ArgumentList "/uninstall /quiet"
+} 
+
+
+$path = UninstallHPEPrintAppSetupInstalled
 
 if($path -ne $null){
-    Write-Host "HPEPrintAppSetup.exe found in" $path.FullName
-    Start-Process $path.FullName -ArgumentList "/uninstall /quiet" -Wait
-    Write-Host HP e-Print removed
+    Write-Host "HPEPrintAppSetup.exe found in" $path.
+    UninstallHPEPrintAppSetupInstalled
+    Start-Sleep -Seconds 5
+    $path = UninstallHPEPrintAppSetupInstalled
+    if($path -eq $null) {
+        Write-Host "HP e-Print removed sucessvol"
+    } else {
+        Write-Host "HP e-Print failed sucessvol"
+        exit 1
+    }
 } else {
     Write-Host Software not found on ([System.Net.DNS]::GetHostByName('').HostName)
 }
+
+
 
