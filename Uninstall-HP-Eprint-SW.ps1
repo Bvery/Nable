5bf2405bde8db88f4adf4aa14eb94c78eb41fe4e
DividedCode <32494831+DividedCode@users.noreply.github.com>
2018-05-23 08:28:16 +0200

    update

--- Uninstall-HP-Eprint-SW.ps1
+++ Uninstall-HP-Eprint-SW.ps1
@@ -1,7 +1,9 @@
 $path = Get-ChildItem -Path "C:\ProgramData\Package Cache\" -Recurse -Filter "HPEPrintAppSetup.exe"
-if($path -ne ""){
+
+if($path -ne $null){
     Start-Process $path.FullName -ArgumentList "/uninstall /quiet"
     Write-Host HP e-Print removed
 } else {
-    Write-Host Software not found.
+    Write-Host Software not found on ([System.Net.DNS]::GetHostByName('').HostName)
 }
+
