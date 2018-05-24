91f7b3e527a366deaad410f942a39f638e286c8d
DividedCode <32494831+DividedCode@users.noreply.github.com>
2018-05-24 10:42:41 +0200

    remove download

--- Uninstall-HP-Eprint-SW.ps1
+++ Uninstall-HP-Eprint-SW.ps1
@@ -1,7 +1,7 @@
-$DownloadPath = 'C:\temp\HPEPrintAppSetup.exe'
+#$DownloadPath = 'C:\temp\HPEPrintAppSetup.exe'
 $SoftwareName = 'HP ePrint SW'
 
-$SourceInput = 'https://github.com/DividedCode/Nable/blob/master/Software%20Resp/HPEPrintAppSetup.exe?raw=true'
+#$SourceInput = 'https://github.com/DividedCode/Nable/blob/master/Software%20Resp/HPEPrintAppSetup.exe?raw=true'
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 #check installed-----------
@@ -17,18 +17,25 @@ if (($32bit -eq $null) -and ($64bit -eq $null)) {
     Write-Host $SoftwareName found on ([System.Net.DNS]::GetHostByName('').HostName)
 }
 
-#Download-----------
+<# #Download-----------
 if (Test-Path $DownloadPath) {
     Write-Host "$DownloadPath already present"
 } else {    
     Write-Host "Start downloading files"
     (New-Object System.Net.WebClient).DownloadFile($SourceInput, $DownloadPath)
-}
-
+} #>
 
 Write-Host "Start uninstall"
 
-Start-Process $DownloadPath -ArgumentList "/quiet /uninstall" -Wait
+foreach ($item in $32bit) {
+    $CmdCommand = "MsiExec.exe /X" + $item.PSChildName + " /quiet"
+    cmd /C $CmdCommand
+}
+foreach ($item in $64bit) {
+    $CmdCommand = $item.UninstallString + " /quiet"
+    cmd /C $CmdCommand
+}
+
 $32bit = $null
 $64bit = $null
 $32bit = get-itemproperty 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' | Select-Object DisplayName, DisplayVersion, UninstallString, PSChildName | Where-Object { $_.DisplayName -match "HP ePrint SW"}
