720b8f62909bbc647af7679760de5384e5d34e27
DividedCode <32494831+DividedCode@users.noreply.github.com>
2018-05-24 12:21:52 +0200

    uninstall-string

--- Uninstall-HP-Eprint-SW.ps1
+++ Uninstall-HP-Eprint-SW.ps1
@@ -9,7 +9,7 @@ $32bit = $null
 $64bit = $null
 $32bit = get-itemproperty 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' | Select-Object DisplayName, DisplayVersion, UninstallString, PSChildName | Where-Object { $_.DisplayName -match "HP ePrint SW"}
 $64bit = get-itemproperty 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*' | Select-Object DisplayName, DisplayVersion, UninstallString, PSChildName | Where-Object { $_.DisplayName -match "HP ePrint SW"}
- 
+
 if (($32bit -eq $null) -and ($64bit -eq $null)) {
     Write-Host $SoftwareName not installed ([System.Net.DNS]::GetHostByName('').HostName)
     exit 0
@@ -27,13 +27,32 @@ if (Test-Path $DownloadPath) {
 
 Write-Host "Start uninstall"
 
+Write-Host "32bit"
 foreach ($item in $32bit) {
-    $CmdCommand = "MsiExec.exe /X" + $item.PSChildName + " /quiet"
-    cmd /C $CmdCommand
+    Write-Host $item
+    if ($item.UninstallString -like "*C:\ProgramData\Package Cache\") {
+        Write-Host "New uninstall string"
+        $CmdCommand = $item.UninstallString
+        cmd /C $CmdCommand
+    } else {
+        Write-Host $item
+        $CmdCommand = "MsiExec.exe /X" + $item.PSChildName 
+        cmd /C $CmdCommand
+    }
 }
+Write-Host $item
+Write-Host "64bit"
 foreach ($item in $64bit) {
-    $CmdCommand = $item.UninstallString + " /quiet"
-    cmd /C $CmdCommand
+    Write-Host $item
+    if ($item.UninstallString -like "*C:\ProgramData\Package Cache\*") {
+        Write-Host "New uninstall string"
+        $CmdCommand = $item.UninstallString + " /quiet"
+        cmd /C $CmdCommand
+    } else {
+        Write-Host $item
+        $CmdCommand = "MsiExec.exe /X" + $item.PSChildName + " /quiet"
+        cmd /C $CmdCommand
+    }
 }
 
 $32bit = $null
